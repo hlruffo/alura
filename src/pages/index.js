f7fa655cd90bbd695dc2269e11e96521fceebe2f
Hugo L. Ruffo <hlruffo@gmail.com>
2022-11-07 20:02:43 -0300

    pages para dentro de src

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import config from "../config.json"
+import config from "../../config.json"
 import styled from "styled-components"
-import { CSSReset } from "../src/components/CSSReset"
-import Menu from "../src/components/Menu"
-import { StyledTimeline } from "../src/components/Timeline"
+import { CSSReset } from "../components/CSSReset"
+import Menu from "../components/Menu"
+import { StyledTimeline } from "../components/Timeline"
 
 
 
