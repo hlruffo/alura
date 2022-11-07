4703e4e0449cfe46b1abc0beaff1352fe465ca68
Hugo L. Ruffo <hlruffo@gmail.com>
2022-11-07 17:10:49 -0300

    tentando cod oficial

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ee2c2797e7c476b"; }
+/******/ 		__webpack_require__.h = function() { return "3a8f567761b6ded6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
