b050ee552d304912bdbbb0dc2c7da26177f40804
Hugo L. Ruffo <hlruffo@gmail.com>
2022-11-07 17:38:56 -0300

    teste3

--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,66 @@
-import config from "../config.json";
-import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
-import Menu from "../src/components/Menu";
-import { StyledTimeline } from "../src/components/Timeline";
+import config from "../config.json"
+import styled from "styled-components"
+import { CSSReset } from "../src/components/CSSReset"
+import Menu from "../src/components/Menu"
+import { StyledTimeline } from "../src/components/Timeline"
+
+
 
 function HomePage() {
-  const estilosDaHomePage = {
-    // backgroundColor: "red" 
-  };
 
-  // console.log(config.playlists);
+  return (<>
+    <CSSReset />
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      flex: 1,
+      // backgroundColor: "red",
+    }}>
 
-  return (
-    <>
-      <CSSReset />
-      <div style={{
-        display: "flex",
-        flexDirection: "column",
-        flex: 1,
-        // backgroundColor: "red",
-      }}>
-        <Menu />
-        <Header />
-        <Timeline playlists={config.playlists}>
-          Conteúdo
-        </Timeline>
-      </div>
-    </>
-  );
+      <Menu />
+      <Header />
+      <Timeline playlists={config.playlists} />
+    </div>
+  </>
+  )
 }
 
 export default HomePage
 
-// function Menu() {
-//     return (
-//         <div>
-//             Menu
-//         </div>
-//     )
-// }
-
-
 const StyledHeader = styled.div`
-    img {
-        width: 80px;
-        height: 80px;
-        border-radius: 50%;
-    }
-    .user-info {
-        margin-top: 50px;
-        display: flex;
-        align-items: center;
-        width: 100%;
-        padding: 16px 32px;
-        gap: 16px;
-    }
+img{
+  width:80px;
+  height:80px;
+  border-radius:50%;
+}
+.user-info{
+  margin-top:50px;;
+  display:flex;
+  align-items: center;
+  width:100%;
+  padding:16px 32px;
+  gap:16px;
+}
 `;
 function Header() {
   return (
     <StyledHeader>
-      {/* <img src="banner" /> */}
+      {/* <img src="banner"></img> */}
       <section className="user-info">
-        <img src={`https://github.com/${config.github}.png`} />
+        <img src={`https://github.com/${config.github}.png`}></img>
         <div>
-          <h2>
-            {config.name}
-          </h2>
-          <p>
-            {config.job}
-          </p>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
         </div>
       </section>
-    </StyledHeader>
-  )
+    </StyledHeader>)
 }
-
-function Timeline(propriedades) {
-  // console.log("Dentro do componente", propriedades.playlists);
-  const playlistNames = Object.keys(propriedades.playlists);
-  // Statement
-  // Retorno por expressão
+function Timeline(props) {
+  const playlistNames = Object.keys(props.playlists)
   return (
     <StyledTimeline>
       {playlistNames.map((playlistName) => {
-        const videos = propriedades.playlists[playlistName];
+        const videos = props.playlists[playlistName];
         console.log(playlistName);
         console.log(videos);
         return (
